7beb9d1898ece2832130168aa4bf364d57fa9d37
patriciaCastillo <patrycast81@gmail.com>
2026-06-19 21:29:28 -0300

    login

--- controller/auth.js
+++ controller/auth.js
@@ -53,17 +53,43 @@ export const confirm= async (req,res) => {
 
 
 
-export const login= async (req, res) => {
-    try {
-        const { email } = req.body;
-        const user= await User.findOne({ email });
-        const token= jwt.sign({ user: user._id }, process.env.JWT_KEY, { expiresIn: "4h" });
-        res.status(200).json({ token });
-    } catch(error) {
-        res.status(500).json({
-            error: error.message
-        })
+// export const login= async (req, res) => {
+//     try {
+//         const { email } = req.body;
+//         const user= await User.findOne({ email });
+//         const token= jwt.sign({ user: user._id }, process.env.JWT_KEY, { expiresIn: "4h" });
+//         res.status(200).json({ token });
+//     } catch(error) {
+//         res.status(500).json({
+//             error: error.message
+//         })
+//     }
+// }
+
+export const login = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
     }
+
+    const token = jwt.sign({ user: user._id }, process.env.JWT_KEY, { expiresIn: "4h" });
+
+    res.status(200).json({ 
+      token,
+      usuario: {
+        id: user._id,
+        nombre: user.name,
+        email: user.email,
+      }
+    });
+  } catch(error) {
+    res.status(500).json({
+      error: error.message
+    })
+  }
 }
 
 
